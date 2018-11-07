22418fdaff363050fb1a8230424335b1d2960776
Cayleigh Verhaalen <verhaacc@dukes.jmu.edu>
2018-11-06 20:02:32 -0500

    Added sprites to the resources package.

--- FinalProject/src/applications/BernstdhBoard.java
+++ FinalProject/src/applications/BernstdhBoard.java
@@ -10,6 +10,8 @@ import app.JApplication;
 import io.ResourceFinder;
 import visual.Visualization;
 import visual.VisualizationView;
+import visual.dynamic.sampled.Screen;
+import visual.statik.SimpleContent;
 import visual.statik.sampled.Content;
 import visual.statik.sampled.ContentFactory;
 
@@ -21,6 +23,10 @@ import visual.statik.sampled.ContentFactory;
  */
 public class BernstdhBoard extends JApplication
 {
+  private SimpleContent[] bernstein_left;
+  private SimpleContent[] bernstein_right;
+  private SimpleContent[] bernstein_erase;
+  
   /**
    * Contructs a new BernstdhBoard of the size width x height.
    * 
@@ -47,6 +53,10 @@ public class BernstdhBoard extends JApplication
     SwingUtilities.invokeAndWait(new BernstdhBoard(1190, 600));
   }
 
+  /**
+   * This method is called just before the main window is first made visible and initializes all of
+   * the components of the window.
+   */
   @Override
   public void init()
   {
@@ -58,11 +68,23 @@ public class BernstdhBoard extends JApplication
     
     ContentFactory factory = new ContentFactory(finder);
     Content bkgd = factory.createContent("maingame_background.png", 4);
+    Content bernstein = factory.createContent("bernstein_default.png", 4);
     Content frgd = factory.createContent("maingame_foreground.png", 4);
+    
+    String[] filesleft = finder.loadResourceNames("bernstein_walkingleft.txt");
+    String[] filesright = finder.loadResourceNames("bernstein_walkingright.txt");
+    String[] fileserase = finder.loadResourceNames("bernstein_erasing.txt");
+    bernstein_left = factory.createContents(filesleft, 4);
+    bernstein_right = factory.createContents(filesright, 4);
+    bernstein_erase = factory.createContents(fileserase, 4);
+    
     bkgd.setScale(1, 1);
+    bernstein.setScale(1, 1);
+    bernstein.setLocation(300, 115);
     frgd.setScale(1, 1);
     
     visual.add(bkgd);
+    visual.add(bernstein);
     visual.add(frgd);
     contentPane.add(view);
   }
